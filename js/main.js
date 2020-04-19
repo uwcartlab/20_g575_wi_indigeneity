f95bd88c484665718c588c1b54192d774525934e
Brody W. Manquen <60198313+BrodyManquen@users.noreply.github.com>
2020-04-18 19:22:25 -0500

    Pseudocode for Choropleth Map

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Pseudocode for Choropleth Map
+// Collect TopoJSON data for US states and Promise data to SetMap() function
+// Translate TopoJSON data with topojson.js
+// Create map svg container and set projection using d3 -- Push translated TopoJSON data (see week 9)
+// Draw Paths from TopoJSON data
+// Join GeoJSON features (States) with CSV data (of state repatriation raw numbers)
+// Create ColorScale (which color? which Classification method?)
+// Color Enumeration Units
+// Create Reexpress Method -- Menu Select that changes Expressed data for each State (different types of artifacts)
+// Recreate Color Scale and Recolor Each Enumeration Unit based on changed Expressed data
+// Create Retrieve Method -- onMouseover or onClick methods
+// Create Dynamic Label with State Name and Number of Returned Artifacts of Chosen Type
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
