137aeab5b1eed83f72cf5e8f202fa25610d03251
Brody W. Manquen <60198313+BrodyManquen@users.noreply.github.com>
2020-04-19 17:33:28 -0500

    Pseudocode for 3 Map Elements
    Pseudocode for all 3 Map elements (except final exploratory tool, which will likely be a combination of all 3)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Pseudocode for Choropleth Map
+
 // Collect TopoJSON data for US states and Promise data to SetMap() function
 // Translate TopoJSON data with topojson.js
 // Create map svg container and set projection using d3 -- Push translated TopoJSON data (see week 9)
@@ -11,8 +12,9 @@
 // Create Retrieve Method -- onMouseover or onClick methods
 // Create Dynamic Label with State Name and Number of Returned Artifacts of Chosen Type
 // Create Dynamic Legend for ColorScale for expressed dataset
-//
+
 // Pseudocode for Flow Map
+
 // Collect TopoJSON data for Wisconsin, Museums/Institutions, and Reservations, and Historical Tribal Bounds and Promise data to SetMap() function
 // Translate TopoJSON data with topojson.js
 // Create map svg container and set projection using d3 -- Push translated TopoJSON data (see week 9)
@@ -26,8 +28,10 @@
 // Create Overlay/Reexpress method to overlay historical tribal bounds when a Reservation/Tribe is selected
 // Create Information Panel update method to dynamically update InfoPanel when new item selected
 // Create (Dynamic?) Legend for Flowline sizes
-//
+// Determine Zoom, Pan, and Search Constraints if we are restricting until end of article
+
 // Pseudocode for Mound Map
+
 // Collect TopoJSON data for Wisconsin, Mounds, and Historical Tribal Bounds and Promise data to SetMap() function
 // Translate TopoJSON data with topojson.js
 // Create map svg container and set projection using d3 -- Push translated TopoJSON data (see week 9)
@@ -37,14 +41,11 @@
 // Create Retrieve via onClick/onMouseover to select Mounds
 // Create Dynamic Label Method for onClick/onMouseover of a mound
 // Create Overlay/Reexpress method to overlay historical tribal bounds
-//
+// Determine Zoom, Pan, and Search Constraints if we are restricting until end of article
+
 // Miscellaneous Pseudocode
+
 // Create divs/styles for Article Information, Titles, Sources, etc --> i.e. all non-Map elements
 // Create Map containers, adjust for screen sizes(?)
-// Coherent Style Scheme --> Styles of Article body and of Map should be compatible. 
-//
-//
-//
-//
-//
-//
+// Coherent Style Scheme --> Styles of Article body and of Map should be compatible.
+// Martini Glass --> Create final element to combine Flow and Mound maps (and all associated features such as Info Panels and Labels) to do exploration at end
