990118a79366876c1dfe3f10dae7bc110190662a
Brody W. Manquen <60198313+BrodyManquen@users.noreply.github.com>
2020-04-20 17:29:32 -0500

    Update Pseudocode from 4/20/2020 Meeting

--- js/main.js
+++ js/main.js
@@ -24,10 +24,11 @@
 // Create Retrieve via onClick/onMouseover to select Tribes or Institutions
 // Create Dynamic Label Method for onClick/onMouseover of a reservation or institution
 // Create Flow Creation Method with Size Scaled to Amount Returned to or from the Tribe or Institution respectively
+// Create Flow Creation Method with Size Scaled to Amount Affiliated to or from the Tribe or Institution respectively -- But not known if Returned (uses dotted line instead of full flow)
 // Create Reexpress method to dynamically call Flow Creation Method when a new Tribe/Institution is selected
 // Create Overlay/Reexpress method to overlay historical tribal bounds when a Reservation/Tribe is selected
 // Create Information Panel update method to dynamically update InfoPanel when new item selected
-// Create (Dynamic?) Legend for Flowline sizes
+// Create Legend for Flowline sizes
 // Determine Zoom, Pan, and Search Constraints if we are restricting until end of article
 
 // Pseudocode for Mound Map
@@ -37,7 +38,8 @@
 // Create map svg container and set projection using d3 -- Push translated TopoJSON data (see week 9)
 // Draw Paths from TopoJSON data
 // Create Information Panel div
-// Create Filter in Menu Selection to filter out Destroyed, Unprotected, and Protected Mounds (checkboxes)
+// Create Filter in Menu Selection to filter out Unprotected and Protected Mounds (checkboxes)
+// Create Reexpress method to color each Mound Site as Destroyed, Partially Intact, Intact, or Unknown
 // Create Retrieve via onClick/onMouseover to select Mounds
 // Create Dynamic Label Method for onClick/onMouseover of a mound
 // Create Overlay/Reexpress method to overlay historical tribal bounds
@@ -48,4 +50,3 @@
 // Create divs/styles for Article Information, Titles, Sources, etc --> i.e. all non-Map elements
 // Create Map containers, adjust for screen sizes(?)
 // Coherent Style Scheme --> Styles of Article body and of Map should be compatible.
-// Martini Glass --> Create final element to combine Flow and Mound maps (and all associated features such as Info Panels and Labels) to do exploration at end
